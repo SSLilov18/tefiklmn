c13bfc862954fb195e42c0102376df43f3246e30
Стивън Лилов <SSLilov18@codingburgas.bg>
2023-01-23 12:26:06 +0200

    Initial commit

---
+++ src/main/java/com/example/tefiklmn/TefiklmnApplication.java
@@ -0,0 +1,13 @@
+package com.example.tefiklmn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TefiklmnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TefiklmnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tefiklmn/TefiklmnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tefiklmn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TefiklmnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
